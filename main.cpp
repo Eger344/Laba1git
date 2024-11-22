1be1c3e0b67252e8616050a72ad0f008d9d86227
Eger344 <chikislov.egor@gmail.com>
2024-11-22 16:51:52 +0500

    Создан каркас

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 
 using namespace std;
+void readFromFile(vector<string>& lines) {
 
-int main()
-{
-    cout << "Hello world!" << endl;
+}
+
+void printLines(const vector<string>& lines) {
+
+}
+
+void writeToFile(const vector<string>& lines) {
+
+}
+
+int main() {
+    vector<string> lines;
+
+    readFromFile(lines);
+    printLines(lines);
+    writeToFile(lines);
+
     return 0;
 }
